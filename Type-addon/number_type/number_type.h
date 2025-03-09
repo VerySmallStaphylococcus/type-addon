c68de180fe84bcfdffd7278b57f6c757fa3b813b
Staphylococcus <softcupcaketeam@gmail.com>
2025-03-09 22:50:18 +0900

    Add files via upload

---
+++ Type-addon/number_type/number_type.h
@@ -0,0 +1,7 @@
+#pragma once
+
+typedef long long TP_int64; //64bit int type
+typedef int TP_int64; //32bit int type
+
+typedef double TP_float64; //64bit float type
+typedef int TP_float32; //32bit float types
---
+++ Type-addon/size_types/size_type.h
@@ -0,0 +1,4 @@
+#pragma once
+
+typedef unsigned long long TP_size64;
+typedef unsigned int TP_size32;
